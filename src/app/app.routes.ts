48c218c43a6095a24111e8c5c9e679ca787595a6
Alfredo Vidueira <vidueira1992@gmail.com>
2024-05-14 15:42:38 -0400

    chore: App component and related files changed to enable router navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.page').then((c) => c.HomePage),
+  },
+  // You can add more routes here
+];
